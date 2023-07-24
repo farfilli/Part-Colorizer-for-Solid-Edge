ffe32409cd632431cd1cedd1db6c150df4248372
Francesco <francesco.arfilli@gmail.com>
2023-07-24 15:34:25 +0200

    Initial commit

---
+++ PartColorizer.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_PartColorizer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form_PartColorizer))
+        Me.BT_Colorize = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.Report = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.CheckFastMode = New System.Windows.Forms.CheckBox()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BT_Colorize
+        '
+        Me.BT_Colorize.Location = New System.Drawing.Point(302, 11)
+        Me.BT_Colorize.Margin = New System.Windows.Forms.Padding(2)
+        Me.BT_Colorize.Name = "BT_Colorize"
+        Me.BT_Colorize.Size = New System.Drawing.Size(121, 35)
+        Me.BT_Colorize.TabIndex = 6
+        Me.BT_Colorize.Text = "Colorize"
+        Me.BT_Colorize.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Report})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 49)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(434, 22)
+        Me.StatusStrip1.SizingGrip = False
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Report
+        '
+        Me.Report.Name = "Report"
+        Me.Report.Size = New System.Drawing.Size(42, 17)
+        Me.Report.Text = "Report"
+        '
+        'CheckFastMode
+        '
+        Me.CheckFastMode.AutoSize = True
+        Me.CheckFastMode.Checked = True
+        Me.CheckFastMode.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckFastMode.Location = New System.Drawing.Point(12, 21)
+        Me.CheckFastMode.Name = "CheckFastMode"
+        Me.CheckFastMode.Size = New System.Drawing.Size(283, 17)
+        Me.CheckFastMode.TabIndex = 8
+        Me.CheckFastMode.Text = "Fast mode (Create FaceStyles but don't apply to faces)"
+        Me.CheckFastMode.UseVisualStyleBackColor = True
+        '
+        'Form_PartColorizer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 71)
+        Me.Controls.Add(Me.CheckFastMode)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.BT_Colorize)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form_PartColorizer"
+        Me.Text = "Part Colorizer"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BT_Colorize As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents Report As ToolStripStatusLabel
+    Friend WithEvents CheckFastMode As CheckBox
+End Class
---
+++ PartColorizer.vb
@@ -0,0 +1,231 @@
+﻿Imports SolidEdgeFramework
+Imports SolidEdgeGeometry
+Imports SolidEdgePart
+
+Public Class Form_PartColorizer
+    Private Sub Form_PartColorizer_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Dim objApp As SolidEdgeFramework.Application
+
+        Try
+            objApp = SolidEdgeCommunity.SolidEdgeUtils.Connect(False, True)
+        Catch ex As Exception
+            MsgBox("Solid Edge must be running")
+            End
+        End Try
+
+        If objApp.Documents.Count = 0 Then
+            MsgBox("A part or sheetmetal document must be open")
+            End
+        End If
+
+        Select Case objApp.ActiveDocumentType
+            Case Is = SolidEdgeFramework.DocumentTypeConstants.igPartDocument, SolidEdgeFramework.DocumentTypeConstants.igSyncPartDocument, SolidEdgeFramework.DocumentTypeConstants.igSheetMetalDocument, SolidEdgeFramework.DocumentTypeConstants.igSyncSheetMetalDocument
+
+            Case Else
+                MsgBox("A part or sheetmetal document must be open")
+                End
+
+        End Select
+
+        Dim objPart As Object
+        objPart = objApp.ActiveDocument
+
+        Dim Models As SolidEdgePart.Models = objPart.Models
+        If Models.Count = 0 Then
+            MsgBox("No design bodies found")
+            End
+        Else
+
+            Dim totFaces As Integer = 0
+            For Each tmpModel As SolidEdgePart.Model In Models
+
+                Dim tmpBody = CType(tmpModel.Body, SolidEdgeGeometry.Body)
+                Dim FaceType = SolidEdgeGeometry.FeatureTopologyQueryTypeConstants.igQueryAll
+                Dim tmpFaces = CType(tmpBody.Faces(FaceType), SolidEdgeGeometry.Faces)
+
+                totFaces += tmpFaces.Count
+
+            Next
+
+            Report.Text = String.Format("Found {0} bodies, {1} faces", Models.Count.ToString, totFaces.ToString)
+
+            'If totFaces > 500 Then
+            '    Report.Text += " (Fast mode recommended)"
+            '    CheckBackground.Checked = True
+            'End If
+
+        End If
+
+        objApp = Nothing
+
+    End Sub
+
+    Private Sub BT_Colorize_Click(sender As Object, e As EventArgs) Handles BT_Colorize.Click
+
+        Dim objApp As SolidEdgeFramework.Application
+
+        Try
+            objApp = SolidEdgeCommunity.SolidEdgeUtils.Connect(False, True)
+        Catch ex As Exception
+            MsgBox("Solid Edge must be running")
+            End
+        End Try
+
+        If objApp.Documents.Count = 0 Then
+            MsgBox("A part or sheetmetal document must be open")
+            End
+        End If
+
+        Select Case objApp.ActiveDocumentType
+            Case Is = SolidEdgeFramework.DocumentTypeConstants.igPartDocument, SolidEdgeFramework.DocumentTypeConstants.igSyncPartDocument, SolidEdgeFramework.DocumentTypeConstants.igSheetMetalDocument, SolidEdgeFramework.DocumentTypeConstants.igSyncSheetMetalDocument
+
+            Case Else
+                MsgBox("A part or sheetmetal document must be open")
+                End
+
+        End Select
+
+        Dim objPart As SolidEdgePart.PartDocument = objApp.ActiveDocument
+        Dim tmpName As String = objPart.FullName
+
+
+        '##### Open in background does't improve performance
+        'If CheckBackground.Checked Then
+        '    objPart.Close(True)
+        '    objPart = SolidEdgeCommunity.Extensions.DocumentsExtensions.OpenInBackground(Of SolidEdgePart.PartDocument)(objApp.Documents, tmpName)
+        'End If
+
+
+        Dim tmpFaceStyles As FaceStyles = objPart.FaceStyles
+
+        Dim Models As SolidEdgePart.Models = objPart.Models
+        If Models.Count = 0 Then
+            MsgBox("No design bodies found")
+            End
+        Else
+            Report.Text = String.Format("Found {0} bodies", Models.Count)
+        End If
+
+        Dim RGBArrayList As New List(Of Double())
+        Dim FaceStyleList As New List(Of FaceStyle)
+        Dim FacesLists As New List(Of List(Of Face))
+
+        Dim i = 0
+
+
+        '##### This switches doesn't improve performance
+        'objApp.ScreenUpdating = False
+        'objApp.Interactive = False
+        'objApp.DelayCompute = True
+
+
+        For Each tmpModel As SolidEdgePart.Model In Models
+            i += 1
+            Report.Text = String.Format("Processing body: ({0}/{1}) {2}", i.ToString, Models.Count.ToString, tmpModel.BodyName)
+
+            Dim tmpBody = CType(tmpModel.Body, SolidEdgeGeometry.Body)
+            Dim FaceType = SolidEdgeGeometry.FeatureTopologyQueryTypeConstants.igQueryAll
+            Dim tmpFaces = CType(tmpBody.Faces(FaceType), SolidEdgeGeometry.Faces)
+
+            Report.Text = String.Format("{0} Faces found", tmpFaces.Count.ToString)
+
+            Dim j = 0
+            For Each tmpFace As SolidEdgeGeometry.Face In tmpFaces
+                j += 1
+
+                If IsNothing(tmpFace.Style) Then
+
+                    Report.Text = String.Format("Processing face {0}/{1} on body {2}/{3}", j.ToString, tmpFaces.Count.ToString, i.ToString, Models.Count.ToString)
+
+                    Dim RVal As Double
+                    Dim GVal As Double
+                    Dim BVal As Double
+                    Dim AVal As Double
+
+                    tmpFace.GetRGBAVals(RVal, GVal, BVal, AVal)
+
+                    Dim RGBArray(3) As Double
+                    RGBArray(0) = RVal
+                    RGBArray(1) = GVal
+                    RGBArray(2) = BVal
+                    RGBArray(3) = AVal
+
+                    Dim tmpFaceStyle As FaceStyle
+                    'Dim tmpList As List(Of Face)
+
+                    If Not RGBArrayList.Any(Function(dd) dd(0) = RVal And dd(1) = GVal And dd(2) = BVal And dd(3) = AVal) Then
+
+                        RGBArrayList.Add(RGBArray)
+
+                        Try '##### ToDo: Check if a style with the same name already exists
+                            tmpFaceStyle = tmpFaceStyles.Add(String.Format("Face style #{0}", CObj(RGBArrayList.Count)), "") 'Tolto stile 'Model Default' perchè non cambia nulla
+                            tmpFaceStyle.DiffuseRed = RVal 'CSng(RVal)
+                            tmpFaceStyle.DiffuseGreen = GVal 'CSng(GVal)
+                            tmpFaceStyle.DiffuseBlue = BVal 'CSng(BVal)
+                            tmpFaceStyle.AmbientRed = RVal 'CSng(RVal)
+                            tmpFaceStyle.AmbientGreen = GVal 'CSng(GVal)
+                            tmpFaceStyle.AmbientBlue = BVal 'CSng(BVal)
+                            tmpFaceStyle.WireframeColorRed = RVal 'CSng(RVal)
+                            tmpFaceStyle.WireframeColorGreen = GVal 'CSng(GVal)
+                            tmpFaceStyle.WireframeColorBlue = BVal 'CSng(BVal)
+
+                            tmpFaceStyle.Opacity = AVal 'CSng(AVal) 'Non è la trasparenza, è 1 anche se colore importato è trasparente
+                            'tmpFaceStyle.Shininess = 0.0F
+                            'tmpFaceStyle.Reflectivity = 0.0F
+                            'tmpFaceStyle.SpecularRed = 0.0F
+                            'tmpFaceStyle.SpecularGreen = 0.0F
+                            'tmpFaceStyle.SpecularBlue = 0.0F
+                            'tmpFaceStyle.EmissionRed = 0.0F
+                            'tmpFaceStyle.EmissionGreen = 0.0F
+                            'tmpFaceStyle.EmissionBlue = 0.0F
+                            FaceStyleList.Add(tmpFaceStyle)
+
+                            'tmpList = New List(Of Face)
+                            'FacesLists.Add(tmpList)
+
+                        Catch ex As Exception
+                        End Try
+
+                    Else
+                        Try '##### ToDo: Check if a style with the same name already exists
+                            tmpFaceStyle = FaceStyleList(RGBArrayList.FindIndex(Function(dd) dd(0) = RVal And dd(1) = GVal And dd(2) = BVal And dd(3) = AVal))
+                        Catch ex As Exception
+                        End Try
+
+                        'tmpList = FacesLists(RGBArrayList.FindIndex(Function(dd) dd(0) = RVal And dd(1) = GVal And dd(2) = BVal And dd(3) = AVal))
+
+                    End If
+
+                    If Not CheckFastMode.Checked Then tmpFace.Style = tmpFaceStyle
+                    'tmpList.Add(tmpFace)
+
+                End If
+
+            Next
+
+        Next
+
+
+
+        '##### Open in background does't improve performance
+        'If CheckBackground.Checked Then
+
+        '    objPart.Close(True)
+        '    objApp.Documents.Open(tmpName)
+
+        'End If
+
+
+        '##### This switches doesn't improve performance
+        'objApp.ScreenUpdating = True
+        'objApp.Interactive = True
+        'objApp.DelayCompute = False
+
+
+        Report.Text = String.Format("Created {0} FaceStyles", FaceStyleList.Count.ToString)
+        objApp = Nothing
+
+    End Sub
+
+End Class
